b71fb099dd74035c72b1c8d88155259f71871913
Bruno Tavares <bruno.tavares@enear.co>
2015-11-04 14:33:22 +0000

    base project (without documentation and tests)

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # browserify-remove-code
 Remove sections of code from files based on conditions (browserify transform)
+
+Props to:
+ - https://github.com/substack/browserify-handbook#writing-your-own
+ - https://github.com/crissdev/gulp-remove-code
+
+TODO:
+ - docs
+ - tests
---
+++ index.js
@@ -0,0 +1,93 @@
+var through = require('through2');
+var extname = require('path').extname;
+
+var regexCache = {};
+
+function escapeForRegExp(str) {
+	return str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+}
+
+function getRemovalTagsRegExp(commentStart, commentEnd, key) {
+	return new RegExp('(' +
+		escapeForRegExp(commentStart) + '\\s*' + escapeForRegExp('removeIf(' + key + ')') + '\\s*' + escapeForRegExp(commentEnd) + '\\s*' +
+		'(\\n|\\r|.)*?' +
+		escapeForRegExp(commentStart) + '\\s*' + escapeForRegExp('endRemoveIf(' + key + ')') + '\\s*' + escapeForRegExp(commentEnd) + ')',
+		'gi');
+}
+
+module.exports = function(options) {
+	options = options || {};
+
+	var conditions = [],
+		commentEnd = '',
+		commentStart;       // not set means autodetect
+
+	Object.keys(options).forEach(function(key) {
+		if (key === 'commentStart') {
+			commentStart = options.commentStart;
+		}
+		else if (key === 'commentEnd') {
+			if (commentStart) {
+				// set it only if commentStart is provided
+				commentEnd = options.commentEnd;
+			}
+			else {
+				gutil.log(gutil.colors.yellow('gulp-remove-code: commentStart was not set but commentEnd provided. ' +
+					'The option will be ignored. commentEnd: ' + commentEnd));
+			}
+		}
+		else if (options[key]) {
+			conditions.push(key);
+		}
+	});
+
+	return function (file) {
+		var fileExt = extname(file);
+
+		var commentStart = '//';
+		var commentEnd = '';
+
+		switch (fileExt) {
+			case 'coffee':
+				commentStart = '#';
+				commentEnd = '';
+				break;
+			case 'css':
+				commentStart = '/*';
+				commentEnd = '*/';
+				break;
+			case 'html':
+				commentStart = '<!--';
+				commentEnd = '-->';
+				break;
+			case 'cshtml':
+				commentStart = '@*';
+				commentEnd = '*@';
+				break;
+			case 'jade':
+				commentStart = '//-';
+				commentEnd = '';
+				break;
+		}
+
+		return through(function (buf, enc, next) {
+			var contents = buf.toString('utf8');
+
+			if (contents.length > 0) {
+				for (var i = 0; i < conditions.length; i++) {
+					var key = conditions[i],
+						regex = regexCache[fileExt + key];
+
+					if (!regex) {
+						regex = regexCache[fileExt + key] = getRemovalTagsRegExp(commentStart, commentEnd, key);
+					}
+					this.push(contents.replace(regex, ''));
+				}
+			} else {
+				this.push(contents);
+			}
+
+			next();
+		});
+	};
+};
